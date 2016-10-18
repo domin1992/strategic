61c65ca65a731d79aee982118fc10245575e7c54
Dominik Nowak <domin1204@gmail.com>
2016-10-18 11:03:23 +0200

    Remove unused directive

--- public/js/controllers/TaskListController.js
+++ public/js/controllers/TaskListController.js
@@ -53,17 +53,3 @@ strategic.controller('TaskListController', function($scope, $routeParams, $http,
     $scope.tasks[index].time_spent_display = TaskService.updateTimeSpentDisplay($scope.tasks[index].time_spent);
   }
 });
-
-strategic.directive('dynamicModel', ['$compile', '$parse', function($compile, $parse){
-  return {
-    restrict: 'A',
-    terminal: true,
-    priority: 100000,
-    link: function (scope, elem) {
-      var name = $parse(elem.attr('dynamic-model'))(scope);
-      elem.removeAttr('dynamic-model');
-      elem.attr('ng-model', name);
-      $compile(elem)(scope);
-    }
-  };
-}]);
